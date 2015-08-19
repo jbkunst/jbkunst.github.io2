036f8520ea79b0ee85c3f93e4438313ad69030be
Joshua <jbkunst@gmail.com>
2015-08-19 15:16:46 -0300

    ading typedjas

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(function(){
+    jQuery("#typed").typed({
+        strings: [
+          "Hello!",
+          "Im a data lover",
+          "Im a data aasdf",
+          "Im a data wrangler",
+          "I love code so much too!",
+          "Thanks for your visit ;)!"
+          ],
+        stringsstops: [0, 8, 10, 0, 0],
+        startDelay: 500,
+        typeSpeed: 100
+      
+    });
+  
+});
