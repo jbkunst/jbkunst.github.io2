8a40e4282dae21c37b7f90ea289682c2c2deaa67
Joshua <jbkunst@gmail.com>
2015-10-08 11:26:59 -0300

    adding helper for widgets

---
+++ _rposts/r_posts_helpers.R
@@ -0,0 +1,11 @@
+iframeFromWidget <- function(wdgt, filename){
+
+  htmlwidgets::saveWidget(wdgt, file = filename, selfcontained = TRUE, libdir = NULL)
+  file.copy(filename, sprintf("htmlwidgets/%s", filename), overwrite = TRUE)
+  file.remove(filename)
+  
+  tplt <- '<iframe src="/htmlwidgets/{{ fn }}" width=100% height=400 allowtransparency="true"></iframe>'
+  
+  whisker::whisker.render(tplt, list(fn = filename ))
+  
+}
