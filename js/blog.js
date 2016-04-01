173183862a511b41dd7f9fae2d4dab1519657e09
Joshua <jbkunst@gmail.com>
2016-04-01 15:59:01 -0300

    updating projects

--- js/blog.js
+++ js/blog.js
@@ -22,14 +22,11 @@ $(document).ready(function() {
     
     $container.isotope({ filter: function() {
       
-      
       var name = $(this).find('.name').text().toLowerCase();
       var category = $(this).find('.category').text().toLowerCase();
     
       var search_box = $("#search_box").val().toLowerCase();
       var category_filter = $("#category_filter").val().toLowerCase();
-      
-      console.log(search_box, category_filter);
   
       if(category_filter == "all"){
         return name.match(search_box);
