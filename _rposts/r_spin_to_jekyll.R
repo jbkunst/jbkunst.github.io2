db237da6c2833d88d95504b15e8489fe7285cfd9
Joshua <jbkunst@gmail.com>
2016-04-13 00:12:12 -0300

    uploading animated choroplets

--- _rposts/r_spin_to_jekyll.R
+++ _rposts/r_spin_to_jekyll.R
@@ -1,7 +1,7 @@
 rm(list = ls())
 library("jbkmisc")
 
-r_script <- "_rposts/2016-01-14-presenting-highcharter.R"
+r_script <- "_rposts/2016-04-12-adding-motion-to-choropleths.R"
 
 # options(pandoc.stack.size = "4048m")
 spin_jekyll_post(r_script)
